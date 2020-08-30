921d9c4c980375ba87d4366d513465c4d393d892
Zyyi <a380652070@gmail.com>
2020-08-31 00:24:26 +0800

    Update auth_demo.sql

--- auth_demo.sql
+++ auth_demo.sql
@@ -1,21 +1,39 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地
+ Source Server         : cloud
  Source Server Type    : MySQL
- Source Server Version : 50731
+ Source Server Version : 50725
  Source Host           : localhost:3306
  Source Schema         : auth_demo
 
  Target Server Type    : MySQL
- Target Server Version : 50731
+ Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 29/08/2020 17:27:04
+ Date: 31/08/2020 00:23:59
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission`  (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+INSERT INTO `t_permission` VALUES (1, '查看当前用户', '/user/current');
+INSERT INTO `t_permission` VALUES (2, '根据ID查询用户', '/user/findById/**');
+INSERT INTO `t_permission` VALUES (3, '保存用户', '/user/save');
 
 -- ----------------------------
 -- Table structure for t_role
@@ -30,6 +48,26 @@ CREATE TABLE `t_role`  (
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
+INSERT INTO `t_role` VALUES (1, 'ADMIN');
+INSERT INTO `t_role` VALUES (2, 'VIP');
+
+-- ----------------------------
+-- Table structure for t_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role_permission`;
+CREATE TABLE `t_role_permission`  (
+  `id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role_permission
+-- ----------------------------
+INSERT INTO `t_role_permission` VALUES (1, 1, 1);
+INSERT INTO `t_role_permission` VALUES (2, 1, 2);
+INSERT INTO `t_role_permission` VALUES (3, 1, 3);
 
 -- ----------------------------
 -- Table structure for t_user
@@ -45,9 +83,10 @@ CREATE TABLE `t_user`  (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (1, 'eric', '123');
-INSERT INTO `t_user` VALUES (1298901526921543681, 'jane', '$2a$10$dZmgjRqchogkR5.FXIyvh./AYXRu22Lsky7BcaKuFgitXU9zHA16C');
-INSERT INTO `t_user` VALUES (1298901673227255809, 'marry', '$2a$10$PPmsv7.H0A69W4uAbN62J.5AdxAihd3P5.jFkaJBAH0lj10tCP6bG');
+INSERT INTO `t_user` VALUES (1298901526921543681, '张大帅', '$2a$10$dZmgjRqchogkR5.FXIyvh./AYXRu22Lsky7BcaKuFgitXU9zHA16C');
+INSERT INTO `t_user` VALUES (1298901673227255809, '张大帅1号', '$2a$10$PPmsv7.H0A69W4uAbN62J.5AdxAihd3P5.jFkaJBAH0lj10tCP6bG');
+INSERT INTO `t_user` VALUES (1300103441425915905, 'kangkang', '$2a$10$y9BL9kVpdXGbFLh6ARcXW.9/.QDqXWSOxCfWIKP6Sx80aIqzTEX6C');
+INSERT INTO `t_user` VALUES (1300105857705783298, 'kangkang', '$2a$10$/mzD5gN9madvYu0bTh456OrUNBOXJd2jZSm2ZsjGmCB6pFeRXcFgW');
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -55,13 +94,15 @@ INSERT INTO `t_user` VALUES (1298901673227255809, 'marry', '$2a$10$PPmsv7.H0A69W
 DROP TABLE IF EXISTS `t_user_role`;
 CREATE TABLE `t_user_role`  (
   `id` bigint(20) NOT NULL,
-  `user_id` int(20) NOT NULL,
-  `role_id` int(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_user_role
 -- ----------------------------
+INSERT INTO `t_user_role` VALUES (1, 1298901526921543681, 1);
+INSERT INTO `t_user_role` VALUES (2, 1298901526921543681, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
